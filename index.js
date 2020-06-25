03834d33b738af6db77a6885c73bbfc47201e557
github username <github email address>
2020-06-24 23:17:38 -0400

    done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function renderPlayer(player) {
   playerDiv.innerHTML = `
     <h3>${player.name} (<em>${player.nickname}</em>)</h3>
     <img src="${player.photo}" alt="${player.name}">
-    <p class="likes">${player.likes} likes</p>
+    <p class="likes"><span>${player.likes}</span> likes</p>
     <button class="like-button">❤️</button>
   `
 
@@ -32,15 +32,51 @@ PLAYERS.forEach(renderPlayer)
 
 
 /***** Deliverable 1 *****/
-function toggleColor(element) {
-  if (element.style.color === "red") {
-    element.style.color = "black"
+let head = document.querySelector("h1#header")
+head.addEventListener('click',function(e) {
+  if (head.style.color === "red") {
+    head.style.color = "black"
   } else {
-    element.style.color = "red"
+    head.style.color = "red"
   }
-}
+})
+
 
 
 /***** Deliverable 2 *****/
+button = document.getElementById("button1")
+button.addEventListener('click',function(e){
+  e.preventDefault()
+ let formS = document.getElementById('new-player-form')
+ const playerDiv = document.createElement("div")
+  // set attributes on the outer div
+  playerDiv.className = "player"
+  playerDiv.dataset.number = formS.number.value
+  let likes = 0;
+  // use innerHTML to create any child elements of the div
+  playerDiv.innerHTML = `
+    <h3>${formS.name.value} (<em>${formS.nickname.value}</em>)</h3>
+    <img src="${formS.photo.value}" alt="${formS.name.value}">
+    <p class="likes"><span>${likes}</span>  likes</p>
+    <button class="like-button">❤️</button>
+  `
 
+  // append the element to the container
+  playerContainer.append(playerDiv)
+  formS.reset();
+})
+
+
+
+//
+
+
+players = document.getElementById('players')
+players.addEventListener('click', function(e){
+  if(e.target.textContent === "❤️"){
+  button = e.target
+  const scoreSpan = button.parentNode.querySelector("span")
+  scoreSpan.textContent = parseInt(scoreSpan.textContent) + 1
+  }
+  })
 /***** Deliverable 3 *****/
